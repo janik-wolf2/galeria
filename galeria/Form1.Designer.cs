5260b2d552d512e2d3f4728096f876e145063bb3
mjaniszewski <mjaniszewski@S3005>
2025-03-11 14:40:12 +0100

    11 marca

--- galeria/Form1.Designer.cs
+++ galeria/Form1.Designer.cs
@@ -28,53 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Wstecz = new System.Windows.Forms.Button();
+            this.Dalej = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Wstecz
             // 
-            this.button1.Location = new System.Drawing.Point(139, 159);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Wstecz.Location = new System.Drawing.Point(117, 187);
+            this.Wstecz.Name = "Wstecz";
+            this.Wstecz.Size = new System.Drawing.Size(95, 48);
+            this.Wstecz.TabIndex = 0;
+            this.Wstecz.Text = "Wstecz";
+            this.Wstecz.UseVisualStyleBackColor = true;
+            this.Wstecz.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Dalej
             // 
-            this.button2.Location = new System.Drawing.Point(539, 159);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 48);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Dalej.Location = new System.Drawing.Point(602, 187);
+            this.Dalej.Name = "Dalej";
+            this.Dalej.Size = new System.Drawing.Size(95, 48);
+            this.Dalej.TabIndex = 1;
+            this.Dalej.Text = "Dalej";
+            this.Dalej.UseVisualStyleBackColor = true;
+            this.Dalej.Click += new System.EventHandler(this.button2_Click);
             // 
-            // openFileDialog1
+            // pictureBox1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.pictureBox1.Image = global::galeria.Properties.Resources.zenek;
+            this.pictureBox1.Location = new System.Drawing.Point(264, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(287, 278);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 74);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zenon Martyniuk";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(0, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(800, 92);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "\"Przez twe oczy zielone oszalalem\"";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Dalej);
+            this.Controls.Add(this.Wstecz);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Wstecz;
+        private System.Windows.Forms.Button Dalej;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- galeria/Form1.cs
+++ galeria/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,19 +13,139 @@ namespace galeria
 {
     public partial class Form1 : Form
     {
+        private OpenFileDialog ofd;
         public Form1()
         {
             InitializeComponent();
         }
+        int licznik = 1;
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            licznik--;
+
+            if (licznik < 1)
+            {
+                licznik = 7;
+            }
+
+            switch (licznik)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.andrzej;
+                    label1.Text = "Andrzej Duda";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.jp2;
+                    label1.Text = "Jan pawel II";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.kubica;
+                    label1.Text = "Robert Kubica";
+                    label2.Text = @"""szybki hehe niby szybki""";
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.lewy;
+                    label1.Text = "Robert Lewandowski";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.maria;
+                    label1.Text = "Maria Skłodowska-Curie";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.pudzian;
+                    label1.Text = "Mariusz Pudzianowski";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.zenek;
+                    label1.Text = "Zenon Martyniuk";
+                    label2.Text = @"""Przez twe oczy zielone""";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            licznik++;
+
+            if (licznik > 7)
+            {
+                licznik = 1;
+            }
+
+            switch (licznik)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.andrzej;
+                    label1.Text = "Andrzej Duda";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.jp2;
+                    label1.Text = "Jan pawel II";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.kubica;
+                    label1.Text = "Robert Kubica";
+                    label2.Text = @"""szybki hehe niby szybki""";
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.lewy;
+                    label1.Text = "Robert Lewandowski";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.maria;
+                    label1.Text = "Maria Skłodowska-Curie";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.pudzian;
+                    label1.Text = "Mariusz Pudzianowski";
+                    label2.Text = @"""Lorem ipsum""";
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.zenek;
+                    label1.Text = "Zenon Martyniuk";
+                    label2.Text = @"""Przez twe oczy zielone""";
+                    break;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                this.pictureBox1.Image = Image.FromFile(ofd.FileName);
+
+            }
+        }
+
+
+ 
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+
+            this.pictureBox1.BorderStyle = BorderStyle.Fixed3D;
         }
     }
 }
